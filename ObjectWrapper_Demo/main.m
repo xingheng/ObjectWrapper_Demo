cadf9067a1d1d050e1099d87c34c10d3abf2ef40
Wei Han <xingheng907@hotmail.com>
2015-04-30 13:25:52 +0800

    Initial Commit

---
+++ ObjectWrapper_Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjectWrapper_Demo
+//
+//  Created by Wei Han on 4/30/15.
+//  Copyright (c) 2015 Wei Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
